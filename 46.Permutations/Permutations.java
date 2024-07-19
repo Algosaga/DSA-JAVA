cf1f9b32616c212551a01f02fb671580358ced58
Moksh parashar <parasharm239@gmail.com>
2024-07-19 13:15:16 +0530

    This is commit is for the leaf creating

--- 46.Permutations/Permutations.java
+++ 46.Permutations/Permutations.java
@@ -2,6 +2,6 @@
 // Creating for the dummy interaction
 public class Permutations{
     public static void main(String[] args) {
-        System.out.println("Say my name");
+        System.out.println("This is a leaf");
     }
 }
