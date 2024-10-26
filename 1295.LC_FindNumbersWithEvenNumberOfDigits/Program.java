2127178b805a83600055c7fd9be179332794a183
Moksh parashar <parasharm239@gmail.com>
2024-10-26 21:38:25 +0530

    Pushed the code for LC problem 1295

---
+++ 1295.LC_FindNumbersWithEvenNumberOfDigits/Program.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Program {
+	public static void main(String[] args){
+		int[] arr = {12,345,2,6};
+		System.out.println(Program.EvenNumbers(arr));
+	}
+	
+	public static int EvenNumbers(int[] arr){
+		ArrayList<String> list = new ArrayList<>();
+		for(int num: arr){
+			String StringedNumber = String.valueOf(num);
+			if(StringedNumber.length() % 2 == 0){
+				list.add(StringedNumber);
+			}
+		}
+		return list.size();
+	}
+}
